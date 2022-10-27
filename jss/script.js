d74124e8d152aebbe4d4926c5d1a9361ce1e20d3
GisellyPereira <giselly.avp@gmail.com>
2022-10-27 18:05:32 -0300

    tooltip refatorada

--- jss/script.js
+++ jss/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './moduless/scroll-suave.js';
 import Accordion from './moduless/accordion.js';
 import TabNav from './moduless/tabnav.js';
 import Modal from './moduless/modal.js';
-import initTooltip from './moduless/tooltip.js';
+import Tooltip from './moduless/tooltip.js';
 import initDropdownMenu from './moduless/dropdown-menu.js';
 import initMenuMobile from './moduless/menu-mobile.js';
 import initFuncionamento from './moduless/funcionamento.js';
@@ -13,16 +13,19 @@ import initAnimacaoScroll from './moduless/scroll-animacao.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-const accordion = new Accordion('[data-anime="accordion"]');
+const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
 const tabnav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabnav.init();
 
-const modal = new Modal('[data-modal="abrir"]','[data-modal="fechar"]','[data-modal="container"]');
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
-initTooltip();
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
